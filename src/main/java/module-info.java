a7145d649f6a721ac1c80ec53c2ad10c5e26a1fb
RoGoLo-05 <robertogomezlopez2005@gmail.com>
2025-02-03 19:58:00 +0100

    Creacion proyecto con java fx y configuración de hibernate

---
+++ src/main/java/module-info.java
@@ -0,0 +1,20 @@
+module org.example.bibliotecafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.bibliotecafx to javafx.fxml;
+    exports org.example.bibliotecafx;
+    exports org.example.bibliotecafx.entities;
+    opens org.example.bibliotecafx.entities to javafx.fxml;
+    exports org.example.bibliotecafx.vista;
+    opens org.example.bibliotecafx.vista to javafx.fxml;
+}
